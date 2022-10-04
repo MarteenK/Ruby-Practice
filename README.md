26c7b426d986f46d9ec87b0ce72ab1fac8d74039
Martin Karlsson <83670863+MarteenK@users.noreply.github.com>
2022-10-04 09:00:59 +0100

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Definiendo-variables
+# Analizando-codigo
---
+++ definiendo-variables/definiendo_variable.rb
@@ -0,0 +1,21 @@
+#Define tres variable tipo entero.
+p edad = 37
+p year = 2022
+p lucky_num = 22
+
+#Define tres variables tipo flotante.
+p flotante_1 = 0,37
+p flotante_2 = -20,22
+p flotante_3 = 22,2222
+
+#Define tres variables tipo boolean.
+p verdadero = true
+p falso = false
+p encendido = true
+
+#Define una variable y asígnale un arreglo que tenga valores tipo string y hash.
+verduleria = [{fruta: "manzana"}, {verdura: "lechuga"}]
+p verduleria
+
+#Define una variable y asígnale un hash con tres claves (keys) de tipo symbol, dos valores tipo arreglo (array) y un valor tipo hash con 3 elementos del tipo de dato que decidas.
+p la_variable = {pais: "Italia", region: "Lombardia", ciudad: "Milano"}, ["Viaje de trabajo", "Viaje de turimos"], {poblacion: 400.000, "Gerundio:" => "Milanes", municipios: 68}
