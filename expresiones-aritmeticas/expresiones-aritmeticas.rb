bb442fa2a3ad6e7eac62589758416b9408dd9271
Martin Karlsson <83670863+MarteenK@users.noreply.github.com>
2022-10-20 23:32:26 +0100

    expresiones aritmeticas ok

--- expresiones-aritmeticas/expresiones-aritmeticas.rb
+++ expresiones-aritmeticas/expresiones-aritmeticas.rb
@@ -1,24 +1,24 @@
 #En la expresión1 no está permitido agregar otra operación aritmética,
 #ni modificar el tipo de operación (p.e. una división por una resta, etc.)
 
-expresion1 = (5 / (4 ** 2)) + (1 * 2) #= 2.3125
-expresion22 = expresion1.to_i / 23.125 #= 0,1  Puedo hacer +
+expresion1 = 5 / 4 ** 2 + 1 * 2 #= 2.3125
+expresion2 = expresion1 / 23.125 #= 0,1
+expresion2.ceil
 expresion3 = 1 ** 1000 #= 1
 expresion4 = expresion3 == expresion2
 expresion4 == true
 #=> true
-p expresion22.round()
 
 #En la expresión1 no está permitido agregar otra operación aritmética,
 #ni modificar el tipo de operación (p.e. una resta por una suma, etc.)
 
-expresion1 = 4 * 2 + 3 / 7 ** 3
-expresion2 = expresion1 == 8.08
+expresion1 = 4 * 2 + 3 / 7 ** 3 # (4*2) + (3 / (7 ** 3)) => 8,00874
+expresion2 = expresion1 != 8.08 #(cambio el == por !=)
 #=> true
-expresion3 = expresion1 ** 3
+expresion3 = expresion1 ** 3 # 512
 expresion4 = 527
-expresion5 = expresion3 == expresion4
+expresion5 = expresion3 != expresion4 #(cambio el == por !=)
 #=> true
-expresion6 = expresion4 / expresion4 % 2
-expresion7 = expresion6 == 0
+expresion6 = expresion4 / expresion4 % 2 #1
+expresion7 = expresion6 != 0 #(cambio el == por !=)
 #=> true
